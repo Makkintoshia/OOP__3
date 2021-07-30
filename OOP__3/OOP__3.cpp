810e7c648b3e3d52b9675e92662fa1a31b1256e1
Makkintoshia <85276507+Makkintoshia@users.noreply.github.com>
2021-07-30 07:33:11 +0300

    Добавьте файлы проекта.

---
+++ OOP__3/OOP__3.cpp
@@ -0,0 +1,78 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Man
+{
+public:
+	
+	int yearbirhday;
+	string fio;
+
+	string sex;
+
+	Man()
+	{
+	
+		sex = "male";
+	
+	}
+
+	
+};
+class Teach
+{
+public:
+
+	int numb;
+	int curse;
+	int sredniy;
+
+	void sredniyball()
+	{
+		int x;
+		int y;
+		int z;
+		cout << "Введите оценку по математики";
+		cin >>  x;
+		cout << "Введите оценку по русскому";
+		cin >> y;
+		cout << "Введите оценку по физики";
+		cin >> z;
+		sredniy = (x + y + z) / 3;
+		cout << sredniy;
+	}
+
+};
+
+class Student :public Man, public Teach
+{
+public:
+	
+	  void show()
+	  {
+		  cout << sex;
+			 
+	  }
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	Man man;
+	Teach teach;
+	Student student;
+	cin >> teach.numb;
+	cin >> teach.curse;
+	cin >> teach.sredniy;
+
+	
+	cout << "Введите год рождения";
+		cin >> man.yearbirhday;
+		cin >> man.fio;
+		student.show();
+}
+
+
